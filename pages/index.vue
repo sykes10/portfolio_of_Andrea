9d556f7692b6bdedf066311a89b8b310fc8a3a6b
Alejandro Arevalo <am01824@rmn.com>
2020-09-29 14:15:56 +0100

    empty return

--- pages/index.vue
+++ pages/index.vue
@@ -101,6 +101,7 @@ export default defineComponent({
         },
       ],
     });
+    return {};
   },
 });
 </script>
--- pages/ticket-tailor.vue
+++ pages/ticket-tailor.vue
@@ -18,6 +18,7 @@ export default defineComponent({
         },
       ],
     });
+    return {};
   },
 });
 </script>
